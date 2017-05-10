48fd55b7b0f02fe73aeadbe5868e4b07058ebc31
Valentin <falinv22@gmail.com>
2017-05-10 22:36:06 +0600

    update to v. 1.3.0
    Обновил контроллеры для каждой сущности из таблиц persons, sites и
    keywords

---
+++ src/main/java/rest/service/api/model/Site.java
@@ -0,0 +1,40 @@
+package rest.service.api.model;
+
+/**
+ * Created by Валентин Фалин on 10.05.2017.
+ */
+public class Site {
+    private int id;
+    private String name;
+    private String base_url;
+    private String open_tag;
+    private String close_tag;
+
+    public Site(int id, String name, String base_url, String open_tag, String close_tag) {
+        this.id = id;
+        this.name = name;
+        this.base_url = base_url;
+        this.open_tag = open_tag;
+        this.close_tag = close_tag;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBase_url() {
+        return base_url;
+    }
+
+    public String getOpen_tag() {
+        return open_tag;
+    }
+
+    public String getClose_tag() {
+        return close_tag;
+    }
+}
